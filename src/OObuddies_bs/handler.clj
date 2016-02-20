7530a4917f4433d9db43e22b970649675b1d58c9
Jared Smith <jared.smith.jrod@gmail.com>
2016-02-20 13:04:36 -0800

    new

--- src/OObuddies_bs/handler.clj
+++ src/OObuddies_bs/handler.clj
@@ -10,6 +10,7 @@
 (defn move
   [request]
   (pprint (:params request)) ;; for debugging
+  (pprint (:params (get request :snakes)))
   {:status 200
    :body {:move "north"
           :taunt "gayyyyymeboyz lmao"}})
@@ -17,7 +18,6 @@
 (defn begin
   [request]
   (pprint (:params request)) ;; for debugging
-  (pprint (:params (get request :snakes)))
   {:status 200
    :body {:name "battlesnake-clojure"
           :color "#ff00ff"
