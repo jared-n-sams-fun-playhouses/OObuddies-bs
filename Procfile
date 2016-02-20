9722f27807c8dc9c1dc349aa3ba64ad73c42ceeb
Jared Smith <jared.smith.jrod@gmail.com>
2016-02-20 11:30:06 -0800

    changed to dylan's template for bs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JVM_OPTS -cp target/OObuddies-bs-standalone.jar clojure.main -m OObuddies-bs.web
+web: lein ring server-headless $PORT
--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject OObuddies-bs "1.0.0-SNAPSHOT"
-  :description "O boi battlesnake"
-  :url "http://oobuddies-bs.herokuapp.com"
-  :license {:name "None"}
+  :description "Clojure starter for Battlesnake 2016"
+  :url "https://github.com/jared-n-sams-fun-playhouses/OObuddies-bs"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]]
-  :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.3.1"]]
-  :hooks [environ.leiningen.hooks]
-  :uberjar-name "OObuddies-bs-standalone.jar"
-  :profiles {:production {:env {:production true}}})
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-json "0.3.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler OObuddies-bs.handler/app
+         :auto-reload? true
+         :auto-refresh? true}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
