c64e758bc470a4b4903ee8fe076284ff65387316
Jared Smith <jared.smith.jrod@gmail.com>
2016-02-20 10:22:49 -0800

    initial commit

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject clojure-getting-started "1.0.0-SNAPSHOT"
-  :description "Demo Clojure web app"
-  :url "http://clojure-getting-started.herokuapp.com"
-  :license {:name "Eclipse Public License v1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+(defproject OObuddies-bs "1.0.0-SNAPSHOT"
+  :description "O boi battlesnake"
+  :url "http://OObuddies-bs.herokuapp.com"
+  :license {:name "None"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]]
